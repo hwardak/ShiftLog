bceb96808288a88eddbf8cc3af44edd3b4d119d4
HWardak <h.wardak@hotmail.com>
2017-09-01 00:58:11 -0400

    Starting time entry, and validation complete.

--- app/src/main/java/hwardak/shiftlog/ShiftFormActivity.java
+++ app/src/main/java/hwardak/shiftlog/ShiftFormActivity.java
@@ -1,13 +1,16 @@
 package hwardak.shiftlog;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -22,13 +25,21 @@ public class ShiftFormActivity extends AppCompatActivity {
 
     EditText employeeNameEditText;
     EditText dateEditText;
-    EditText declaredStartTimeEditText;
-    EditText declaredEndTimeEditText;
+    EditText declaredStartTimeHourEditText;
+    EditText declaredStartTimeMinuteEditText;
+    EditText declaredEndTimeHourEditText;
+    EditText declaredEndTimeMinuteEditText;
     EditText startingTillAmountEditText;
     EditText finalDropAmountEditText;
     EditText redemptionsEditText;
     TextView infoBannerTextView;
+
     RadioGroup tillNumberRadioGroup;
+    RadioButton tillOneRadioButton;
+    RadioButton tillTwoRadioButton;
+    RadioButton tillOverLapRadioButton;
+    ToggleButton startTimeAmPmToggleButton;
+    ToggleButton endTimeAmPmToggleButton;
 
     boolean employeeHasOpenShift;
 
@@ -101,14 +112,34 @@ public class ShiftFormActivity extends AppCompatActivity {
 
         employeeNameEditText = (EditText) findViewById(R.id.employeeNameEditText);
         dateEditText = (EditText) findViewById(R.id.dateEditText);
-        declaredStartTimeEditText = (EditText) findViewById(R.id.startTimeEditText);
-//        declaredEndTimeEditText = (EditText) findViewById(R.id.endTimeEditText);
+        declaredStartTimeHourEditText = (EditText) findViewById(R.id.startTimeHourEditText);
+        declaredStartTimeMinuteEditText = (EditText) findViewById(R.id.startTimeMinuteEditText);
+        declaredEndTimeHourEditText = (EditText) findViewById(R.id.endTimeHourEditText);
+        declaredEndTimeMinuteEditText = (EditText) findViewById(R.id.endTimeMinuteEditText);
         startingTillAmountEditText = (EditText) findViewById(R.id.startingTillAmountEditText);
         finalDropAmountEditText = (EditText) findViewById(R.id.finalDropAmountEditText);
         redemptionsEditText = (EditText) findViewById(R.id.redemptionsEditText);
-        tillNumberRadioGroup = (RadioGroup) findViewById(R.id.tillNumberRadioGroup);
         infoBannerTextView = (TextView) findViewById(R.id.infoBannerTextView);
+        startTimeAmPmToggleButton = (ToggleButton) findViewById(R.id.startTimeAmPmToggleButton);
+        endTimeAmPmToggleButton = (ToggleButton) findViewById(R.id.endTimeAmPmToggleButton);
 
+        tillNumberRadioGroup = (RadioGroup) findViewById(R.id.tillNumberRadioGroup);
+        tillOneRadioButton = (RadioButton) findViewById(R.id.tillOneRadioButton);
+        tillTwoRadioButton = (RadioButton) findViewById(R.id.tillTwoRadioButton);
+        tillOverLapRadioButton = (RadioButton) findViewById(R.id.tillOverLapRadioButton);
+
+
+        tillNumberRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                Log.d("TillOverLap", " " + tillOverLapRadioButton.getId());
+                Log.d("checkedId", " " + checkedId);
+                if(tillOverLapRadioButton.getId() == checkedId){
+                    Log.d("TillOverLapToggled", " ");
+                }
+
+            }
+        });
 
     }
 
@@ -125,7 +156,7 @@ public class ShiftFormActivity extends AppCompatActivity {
         employeeNameEditText.setText(employeeName);
 
         declaredStartTime = shiftData.get(1);
-        declaredStartTimeEditText.setText(declaredStartTime);
+//        declaredStartTimeEditText.setText(declaredStartTime);
 
         date = shiftData.get(2);
         dateEditText.setText(date);
@@ -155,86 +186,151 @@ public class ShiftFormActivity extends AppCompatActivity {
     }
 
 
-    private void checkOpeningFormFields() {
+
+    private boolean checkOpeningFormFields() {
+        boolean pass = true;
+        infoBannerTextView.setText("");
+
+        if(tillOverLapRadioButton.isChecked()){
+            return true;
+        }
+
+        if(isDeclaredStartTimeValid()){
+            declaredStartTime = getDeclaredStartTime();
+        } else {
+            pass = false;
+        }
+
+        if(isStartingTillValid()){
+            startingTillAmount = Double.parseDouble(startingTillAmountEditText.getText().toString());
+        } else {
+            pass = false;
+        }
+
+        return pass;
+    }
+
+    private boolean isShiftOverLap() {
+        RadioButton tillNumber = (RadioButton) findViewById(tillNumberRadioGroup.getCheckedRadioButtonId());
+        Log.d("TillNumber", tillNumber.getText().toString());
+
+        if(tillNumber.getText().toString().equals("Over Lap")){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private String formatTimeTo24h(String time) {
+
+        return time;
 
     }
 
-//    //TODO: add zero to start of time if length is 3.
-//    private boolean isDeclaredStartTimeValid() {
-//        declaredStartTime = declaredStartTimeEditText.getText().toString();
-//        String formErrors = "";
-//        int hour;
-//        int minute;
-//        boolean formValidationPass = true;
-//
-//        Log.d("declaredStart", declaredStartTime);
-//
-//        //remove semicolon.
-//        declaredStartTime = declaredStartTime.replace(":", "");
-//
-//        //Check if declared time is NULL and between 3 and 4 chars in length.
-//        if (declaredStartTime != null && (declaredStartTime.length() >= 3 && declaredStartTime.length() <= 4)) {
-//
-//            //If the declaredStartTime length is, a zero will be added to the begining of the String.
-//            // 930 = 0930
-//            if (declaredStartTime.length() == 3) {
-//                declaredStartTime = "0" + declaredStartTime;
-//            }
-//
-//            hour = Integer.parseInt(declaredStartTime.substring(0, 2));
-//            minute = Integer.parseInt(declaredStartTime.substring(2));
-//
-//            Log.d("hour", " " + hour);
-//            Log.d("min", " " + minute);
-//
-//            //check if hour is valid, greater than 0 and less than 24.
-//            if (!(hour >= 0 && hour < 24)) {
-//                formErrors += "Starting time invalid HOUR\n";
-//                formValidationPass = false;
-//            }
-//
-//            //check if minute is valid, greater than 0, and less than 59.
-//            if (!(minute >= 0 && minute < 59)) {
-//                formErrors += "Starting time invalid MINUTE\n";
-//                formValidationPass = false;
-//
-//            }
-//        } else {
-//            formValidationPass = false;
-//            formErrors += "Invalid starting time format.\n";
-//        }
-//
-//        if(formValidationPass){
-//            declaredStartTimeEditText.setText(declaredStartTime);
-//        }
-//        updateInfoBanner(formErrors);
-//
-//        return formValidationPass;
-//
-//    }
+    private boolean isDeclaredStartTimeValid() {
+        boolean pass = true;
+        String formErrors = "";
 
-    private void updateInfoBanner(String formErrors) {
-        infoBannerTextView.setText(formErrors);
+        //Check if the hour EditText is not empty.
+        if(!declaredStartTimeHourEditText.getText().toString().equals("")){
+            //Check if the hour EditText value is less than 12, since we will be using the AmPm format.
+            if(Integer.parseInt(declaredStartTimeHourEditText.getText().toString()) > 12 ){
+                pass = false;
+                formErrors += "Invalid Starting Hour.\n";
+            }
+        } else {
+            pass = false;
+            formErrors += "Enter starting time hour.";
+
+        }
+
+        //Check if the minute EditText is not empty.
+        if(!declaredStartTimeMinuteEditText.getText().toString().equals("")) {
+            //Check if the minute EditText value is less than 59.
+            if (Integer.parseInt(declaredStartTimeMinuteEditText.getText().toString()) > 59) {
+                pass = false;
+                formErrors += "Invalid Starting Minute.\n";
+            }
+        } else {
+            pass = false;
+            formErrors += "Enter starting time minute";
+        }
+
+        //If the minute edittext only contains only one integer, a zero will be added before the
+        //integer.  2 >> 02
+        if(declaredStartTimeMinuteEditText.getText().toString().length() == 1){
+            String currentStartTimeMinute = declaredStartTimeMinuteEditText.getText().toString();
+            String newStartTimeMinute = "0" + currentStartTimeMinute;
+            declaredStartTimeMinuteEditText.setText(newStartTimeMinute);
+        }
+
+        //If the validation was a fail, info banner will be updated with a list of all form errors.
+        if(!pass){
+            updateInfoBanner(formErrors);
+        }
+
+        return pass;
     }
+
+
+
+
 
     //TODO: add one to two zeros to end of starting till
     public boolean isStartingTillValid() {
-        String formErrors;
-        if (!startingTillAmountEditText.getText().toString().equals("")) {
-            if(!startingTillAmountEditText.getText().toString().contains(".")){
-                String startingTillPlusDecimalAndZeros = startingTillAmountEditText.getText().toString() + ".00";
-                startingTillAmountEditText.setText(startingTillPlusDecimalAndZeros);
-            }
-            startingTillAmount = Double.parseDouble(startingTillAmountEditText.getText().toString());
-            Log.d("Starting Till", " " + startingTillAmount);
+        String formErrors = "";
+        boolean pass = true;
 
-            if (startingTillAmount > 0) {
-                return true;
+        //If starting till is NOT empty.
+        if (!startingTillAmountEditText.getText().toString().equals("")) {
+            String startingTill = startingTillAmountEditText.getText().toString();
+
+            //If starting till contains a decimal.
+            if (startingTill.contains(".")) {
+
+                //If no chars after decimal, add '00'
+                if (startingTill.substring(startingTill.indexOf(".")).length() == 1) {
+                    startingTill += "00";
+                }
+
+                //If only 1 char after decimal, add '0'
+                if (startingTill.substring(startingTill.indexOf(".")).length() == 2) {
+                    startingTill += "0";
+                }
+
+                //No decimal, add '.00'
+            } else {
+                startingTill += ".00";
             }
+            //Update starting till EditText with formatted starting till value.
+            startingTillAmountEditText.setText(startingTill);
+
+            Log.d("Starting Till", " " + startingTill);
+
+        } else {
+            formErrors += "Starting Till Invalid.";
+            updateInfoBanner(formErrors);
+            pass = false;
         }
-//        formErrors += "Invalid Starting Till";
-        return false;
+        return pass;
+    }
+
+    public String getDeclaredStartTime() {
+        String timeAmPm = "";
+        timeAmPm
+                = declaredStartTimeHourEditText.getText().toString()
+                + declaredStartTimeMinuteEditText.getText().toString()
+                + startTimeAmPmToggleButton.getText().toString();
+
+        Log.d("timeAmPm", timeAmPm);
+
+        return timeAmPm;
     }
 
 
+    private void updateInfoBanner(String formErrors) {
+        String currentBannerText = infoBannerTextView.getText().toString();
+        String newBannerText = currentBannerText + formErrors;
+        infoBannerTextView.setText(newBannerText);
+    }
 }
