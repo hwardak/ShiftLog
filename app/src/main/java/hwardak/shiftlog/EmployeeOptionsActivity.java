005699fdd665a34dfc9cc83c55685fe347a665d1
HWardak <h.wardak@hotmail.com>
2017-12-15 22:26:22 -0500

    Restructure methods for better user readability, and update comments.

--- app/src/main/java/hwardak/shiftlog/EmployeeOptionsActivity.java
+++ app/src/main/java/hwardak/shiftlog/EmployeeOptionsActivity.java
@@ -26,21 +26,25 @@ public class EmployeeOptionsActivity extends AppCompatActivity {
     EmployeeDataAccess employeeDataAccess = new EmployeeDataAccess(this);
 
     Button deleteEmployeeButton;
+
+    EditText employeeIdEditText;
+    EditText employeeNameEditText;
+
     ListView listView;
     ListAdapter listAdapter;
     ArrayList<String> employeeList;
-    int ListViewPosition;
+
     View rowView;
     View previouslySelectedView;
     LinearLayout addEmployeeLayout;
-    EditText employeeIdEditText;
-    EditText employeeNameEditText;
+
+    int ListViewPosition;
     int id;
     String name;
 
 
     /**
-     * 
+     * Creates activity and invokes methods to instantiate views.
      * @param savedInstanceState
      */
     @Override
@@ -50,38 +54,60 @@ public class EmployeeOptionsActivity extends AppCompatActivity {
 
         updateListView();
 
-        addEmployeeLayout = (LinearLayout) findViewById(R.id.employeeOptionsAddEmployeeLinearLayout);
-        deleteEmployeeButton = (Button) findViewById(R.id.deleteEmployeeButton);
-        employeeIdEditText = (EditText) findViewById(R.id.employeeOptionsIdEditText);
-        employeeNameEditText = (EditText) findViewById(R.id.employeeOptionsNameEditText);
+        instantiateViews();
 
+        instantiateListViewClickListener();
+
+
+    }
+
+    /**
+     * On click listener on the ListView of employees. If an item in the ListView is clicked, it is
+     * highlighted, and the previous selection is un-highlighted. The first time an item is selected
+     * the deleteEmployeeButton is enabaled.
+     */
+    private void instantiateListViewClickListener() {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
 
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
                 deleteEmployeeButton.setEnabled(true);
                 rowView = view;
                 ListViewPosition = position;
+                //If there is a previously selected view in the list, it will be un-highlighted when
+                //a new view within the list is selected.
                 if (previouslySelectedView != null) {
                     previouslySelectedView.setBackgroundColor(0);
                     previouslySelectedView = view;
                 } else {
                     previouslySelectedView = view;
-
                 }
-
+                //Clicked view in list will be highlighted.
                 view.setBackgroundColor(Color.parseColor("#66ccff"));
 
 
             }
         });
-
-
     }
 
-    public void onDeleteButtonClick(View view) {
+    /**
+     * All views in this activity will be instantiated within this method.
+     */
+    private void instantiateViews() {
+        addEmployeeLayout = (LinearLayout) findViewById(R.id.employeeOptionsAddEmployeeLinearLayout);
+        deleteEmployeeButton = (Button) findViewById(R.id.deleteEmployeeButton);
+        employeeIdEditText = (EditText) findViewById(R.id.employeeOptionsIdEditText);
+        employeeNameEditText = (EditText) findViewById(R.id.employeeOptionsNameEditText);
+    }
 
+
+    /**
+     * Button to delete employees from the list. Invokes the deleteEmployee() from the employee data
+     * access layer.
+     * @param view The item in the ListView that is selected.
+     */
+    public void onDeleteButtonClick(View view) {
+        //User is prompted to confirm the deletion of the selected employee.
         new AlertDialog.Builder(this)
                 .setTitle("Delete User")
                 .setMessage("Confirm delete user \"" + employeeList.get(ListViewPosition).substring(0, employeeList.get(ListViewPosition).indexOf(" ")) + "\"")
@@ -92,28 +118,36 @@ public class EmployeeOptionsActivity extends AppCompatActivity {
                         employeeDataAccess.deleteEmployee(employeeList.get(ListViewPosition)
                                 .substring(0, employeeList.get(ListViewPosition).indexOf(" ")));
 
+                        //The row index that was just deleted is un-highlighted.
                         rowView.setBackgroundColor(0);
-                        ((BaseAdapter) listAdapter).notifyDataSetChanged();
-
                         previouslySelectedView = null;
 
-
+                        //Update the ListView with the new list view.
+                        ((BaseAdapter) listAdapter).notifyDataSetChanged();
                         updateListView();
+
+                        //Disable delete button since there is no longer a selection.
                         deleteEmployeeButton.setEnabled(false);
 
                     }
                 })
+
+                //Do nothing if the no if user wishes to NOT delete the selection.
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // do nothing
                     }
                 })
+
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
 
 
     }
 
+    /**
+     * Refresh the listview.
+     */
     private void updateListView() {
         employeeList = employeeDataAccess.getEmployeeList();
         listAdapter = new ArrayAdapter<>(this, R.layout.employee_listview_row, R.id.listViewRow, employeeList);
@@ -122,16 +156,22 @@ public class EmployeeOptionsActivity extends AppCompatActivity {
 
     }
 
-    private ArrayList<String> getEmployeeList() {
-        employeeList = employeeDataAccess.getEmployeeList();
-        return employeeList;
-    }
+
+//    private ArrayList<String> getEmployeeList() {
+//        employeeList = employeeDataAccess.getEmployeeList();
+//        return employeeList;
+//    }
 
 
+    /**
+     * On click to add new employee to list.
+     * @param view
+     */
     public void addEmployeeButtonOnClick(View view) {
 
-
-        if (validateIdAndName()) {
+        //Check if the name and Id in the editTexts are acceptable.
+        if (isNameAndIdValid()) {
+            //Clear fields and reset hints.
             employeeIdEditText.setText("");
             employeeIdEditText.setHint("Enter Id...");
             employeeNameEditText.setText("");
@@ -151,26 +191,39 @@ public class EmployeeOptionsActivity extends AppCompatActivity {
         }
     }
 
-    private boolean validateIdAndName() {
+    /**
+     * Check if the values in the employeeIdEditText and employeeNameEditText are acceptable.
+     * If not the user is informed via highlighed hints.
+     * @return
+     */
+    private boolean isNameAndIdValid() {
 
-        if (employeeIdEditText.getText().toString().length() == 0) {
+        //Check if the employeeIdEditText has a value. Returns false if not.
+        if (employeeIdEditText.getText().toString().trim().length() == 0) {
             employeeIdEditText.setHintTextColor(Color.parseColor("RED"));
             return false;
         }
-        if (employeeNameEditText.getText().toString().length() == 0) {
+
+        //Check if the employeeNameEditText has a value. Returns false if not.
+        if (employeeNameEditText.getText().toString().trim().length() == 0) {
             employeeNameEditText.setHintTextColor(Color.parseColor("RED"));
             return false;
         }
 
-        id = Integer.parseInt(employeeIdEditText.getText().toString());
-        name = employeeNameEditText.getText().toString();
 
-        if(employeeDataAccess.doesEmployeeExist(id)) {
+        //Check if the employee id already already exists in the employee table.
+        if(employeeDataAccess.doesEmployeeExist(Integer.parseInt(employeeIdEditText.getText().toString().trim()))) {
             employeeIdEditText.setText("");
             employeeIdEditText.setHint("Choose another ID");
             employeeIdEditText.setHintTextColor(Color.parseColor("RED"));
             return false;
         }
+
+
+        //If this method has not returned false yet, below variables are assigned accordingly.
+        id = Integer.parseInt(employeeIdEditText.getText().toString().trim());
+        name = employeeNameEditText.getText().toString();
+
 
         return true;
     }
